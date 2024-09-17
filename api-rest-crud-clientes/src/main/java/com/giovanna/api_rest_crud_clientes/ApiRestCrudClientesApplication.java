17fa3c8af6b6f5618c1f999808a837bf7b08c8da
Giovanna Borges <giovannaborgesmassi@gmail.com>
2024-09-17 14:12:53 -0300

    add projeto spring

---
+++ api-rest-crud-clientes/src/main/java/com/giovanna/api_rest_crud_clientes/ApiRestCrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.giovanna.api_rest_crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCrudClientesApplication.class, args);
+	}
+
+}
---
+++ api-rest-crud-clientes/src/test/java/com/giovanna/api_rest_crud_clientes/ApiRestCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanna.api_rest_crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
