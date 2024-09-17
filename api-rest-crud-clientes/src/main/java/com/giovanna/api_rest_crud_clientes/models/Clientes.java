facebfd8a9b0c902ba495f9ba1546b8edf7e2b37
Giovanna Borges <giovannaborgesmassi@gmail.com>
2024-09-17 16:37:48 -0300

    entidade e atributos adicionados

--- api-rest-crud-clientes/src/main/java/com/giovanna/api_rest_crud_clientes/models/Clientes.java
+++ api-rest-crud-clientes/src/main/java/com/giovanna/api_rest_crud_clientes/models/Clientes.java
@@ -1,5 +1,28 @@
 package com.giovanna.api_rest_crud_clientes.models;
 
-public class Clientes {
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.Serializable;    //java pode converter a classe em objeto
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cliente")
+@EqualsAndHashCode(of = "id")
+public class Clientes implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     //valor será incrementado automaticamente
+    private Long id;
+
+    @Column(nullable = false)   //não pode ter valor nulo
+    private String name;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String address;
 }
